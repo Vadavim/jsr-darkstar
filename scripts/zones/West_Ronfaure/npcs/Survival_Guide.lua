c7210bd5504604d6e3e5cf7ebee806bd48f52314
Vadavim <vadavim@gmail.com>
2017-01-17 22:11:09 -0500

    Summoner changes

--- scripts/zones/West_Ronfaure/npcs/Survival_Guide.lua
+++ scripts/zones/West_Ronfaure/npcs/Survival_Guide.lua
@@ -3,37 +3,16 @@
 -- NPC:  Field Manual    
 -----------------------------------    
     
-require("scripts/globals/settings");
-require("scripts/globals/status");
---require("scripts/globals/fieldsofvalor");
+require("scripts/globals/settings");    
+require("scripts/globals/fieldsofvalor");    
     
 -----------------------------------    
 -- onTrigger Action    
------------------------------------
-
-function parseLocsSay(player, locs)
-    local sayString = "";
-    for i,v in pairs(locs) do
-        sayString = sayString .. v[1] .. " (" .. v[2] .. " x" .. tostring(v[3]) .. ")\n";
-    end
-
-    player:SayToPlayer(sayString);
-
-end
-
-
-function onTrigger(player,npc)
-    local locs = {
-        {"King Ranp. Tomb", "Earth", 4},
-        {"Bost. Oubliette", "Water", 4},
-        {"Forst Ghelsba", "Fire", 4},
-        {"La Theine", "Wind", 4}
-    };
---    parseLocsSay(player, locs);
---    player:addStatusEffect(EFFECT_ATMA,1,3,60);
---    startFov(FOV_EVENT_WEST_RONFAURE,player);
---    player:SayToPlayer("King Ranperre's Tomb (x4 Earth)\nBost. Oubliette (x4 Water)\nFort Ghelsba(x4 Fire)\n")
-end;
+-----------------------------------    
+    
+function onTrigger(player,npc)    
+    startFov(FOV_EVENT_WEST_RONFAURE,player);
+end;    
     
 -----------------------------------    
 -- onTrade Action    
@@ -47,7 +26,7 @@ end;
 -----------------------------------    
     
 function onEventUpdate(player,csid,menuchoice)    
---    updateFov(player,csid,menuchoice,1,2,3,4,56);
+    updateFov(player,csid,menuchoice,1,2,3,4,56);
 end;    
     
 -----------------------------------    
